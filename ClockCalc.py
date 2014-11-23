ee655e8c011c4301e377331bcf0930cc6224fe4a
softgarden137 <softgarden@globe.ocn.ne.jp>
2014-11-23 18:56:19 +0900

    pages commit

--- ClockCalc.py
+++ ClockCalc.py
@@ -182,14 +182,86 @@ def Walk(tree):
         for x in Walk(tree.Content):
             yield x
 
+def enliven(w):
+    try:
+        controls = [ n for n in Walk(w) if isinstance(n, Button) or isinstance(n, TextBox) ]
+        Calculator(controls)
+    except:
+        print "Function failed. Did you pass in the Calculator window?"
+
+class Calculator:
+
+    def __init__(self, controls):
+        self.expression = ""
+
+        for i in controls:
+            if isinstance(i, Button):
+                if hasattr(self, "on_" + i.Name):
+                    print "Registering self.on_" + i.Name + " to handle " + i.Name + ".Click"
+                    i.Click += getattr(self, "on_" + i.Name)
+            elif isinstance(i, TextBox):
+                if i.Name == "Result":
+                    self.result = i
+        self.result.Text = self.expression
+        
+
+    def on_Button(self, c):
+        self.expression += c
+        self.result.Text = self.expression
+
+    def on_Clear(self, b, e):
+        self.expression = ""
+        self.result.Text = self.expression
+
+    def on_Equals(self, b, e):
+        try:
+            result = str(eval(self.expression))
+            self.result.Text = result
+            self.expression = result
+        except:
+            self.result.Text = "<<ERROR>>"
+            self.expression = ""
+
+    def on_One(self, b, e):
+        self.on_Button('1')
+    def on_Nine(self, b, e):
+        self.on_Button('9')
+    def on_Eight(self, b, e):
+        self.on_Button('8')
+    def on_Five(self, b, e):
+        self.on_Button('5')
+    def on_Four(self, b, e):
+        self.on_Button('4')
+    def on_Two(self, b, e):
+        self.on_Button('2')
+    def on_Three(self, b, e):
+        self.on_Button('3')
+    def on_Six(self, b, e):
+        self.on_Button('6')
+    def on_Multiply(self, b, e):
+        self.on_Button('*')
+    def on_Seven(self, b, e):
+        self.on_Button('7')
+    def on_Subtract(self, b, e):
+        self.on_Button('-')
+    def on_Zero(self, b, e):
+        self.on_Button('0')
+    def on_DecimalPoint(self, b, e):
+        self.on_Button('.')
+    def on_Plus(self, b, e):
+        self.on_Button('+')
+    def on_Divide(self, b, e):
+        self.on_Button('/')
+
 class Calc(Object):
     def __init__(self):
         #self.Content = LoadXaml('calc.xaml')
         self.Content = XamlReader.Load(_Calc_xaml_str)
         controls = [ n for n in Walk(self.Content) if isinstance(n, Button) or isinstance(n, TextBox) ]
         for c in controls: c.FontSize *=2
-        import calculator
-        calculator.enliven(self.Content)
+        #import calculator
+        #calculator.enliven(self.Content)
+        enliven(self.Content)
 
 class Clock(Object):
     def __init__(self):
