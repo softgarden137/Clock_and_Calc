1d568f10eb1ad6b658b874304b1e9d26c59335e3
Maeda <softgarden@globe.ocn.ne.jp>
2014-11-23 23:26:34 +0900

    Update ClockCalc.py

--- ClockCalc.py
+++ ClockCalc.py
@@ -182,6 +182,7 @@ def Walk(tree):
         for x in Walk(tree.Content):
             yield x
 
+_test_str = """
 def enliven(w):
     try:
         controls = [ n for n in Walk(w) if isinstance(n, Button) or isinstance(n, TextBox) ]
@@ -252,6 +253,7 @@ class Calculator:
         self.on_Button('+')
     def on_Divide(self, b, e):
         self.on_Button('/')
+"""
 
 class Calc(Object):
     def __init__(self):
@@ -259,9 +261,9 @@ class Calc(Object):
         self.Content = XamlReader.Load(_Calc_xaml_str)
         controls = [ n for n in Walk(self.Content) if isinstance(n, Button) or isinstance(n, TextBox) ]
         for c in controls: c.FontSize *=2
-        #import calculator
-        #calculator.enliven(self.Content)
-        enliven(self.Content)
+        import calculator
+        calculator.enliven(self.Content)
+        #enliven(self.Content)
 
 class Clock(Object):
     def __init__(self):
